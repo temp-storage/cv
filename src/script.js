7c63bf75f236429375b28183291abd0a6086e366
PyryV <pyry.vehniainen@gmail.com>
2022-09-28 13:41:43 +0300

    Updates to code, inclusion of a card

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function card1f() 
+{
+    if (document.getElementById("card1").style.visibility == "visible")
+    {
+        document.getElementById("card1").style.visibility = "hidden";
+    }
+    else
+    {
+        document.getElementById("card1").style.visibility = "visible";
+    }
+}
