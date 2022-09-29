560031cff1f14188d089704a9d02e3be42d1c6b4
PyryV <pyry.vehniainen@gmail.com>
2022-09-29 10:24:04 +0300

    Added more cards, JavaScript and functionality.

--- src/script.js
+++ src/script.js
@@ -6,6 +6,20 @@ function card1f()
     }
     else
     {
+        document.getElementById("card2").style.visibility = "hidden";
         document.getElementById("card1").style.visibility = "visible";
+    }
+}
+
+function card2f() 
+{
+    if (document.getElementById("card2").style.visibility == "visible")
+    {
+        document.getElementById("card2").style.visibility = "hidden";
+    }
+    else
+    {
+        document.getElementById("card1").style.visibility = "hidden";
+        document.getElementById("card2").style.visibility = "visible";
     }
 }
