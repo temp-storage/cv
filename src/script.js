107f4b2de5c1bc26693043dd6f269eef30d15316
PyryV <pyry.vehniainen@gmail.com>
2022-09-29 12:10:04 +0300

    More card text

--- src/script.js
+++ src/script.js
@@ -22,12 +22,17 @@ function card0f(num)
             let year = T.getFullYear();
             year = year - 1997;
             document.getElementById("card0").innerHTML =
-            'Hello, my name is Pyry Vehniäinen. I am a '+ year +' years old software developer. <br> This is my portfolio website. <br> Feel free to look around. <br>';
+            'Hello, my name is Pyry Vehniäinen. <br> I am a '+ year +' years old software developer. <br> This is my portfolio website. <br> Feel free to look around. <br>';
             break;
 
         case 2:
-            document.getElementById("card0").innerHTML = 'I know how to use Git / Github in my projects. <br> And I have done coding projects using: <ul> <li>C / C++</li> <li>C#</li> <li>JavaScript</li> <li>Python</li> <li>HTML / CSS</li> </ul>'
+            document.getElementById("card0").innerHTML = 'I know how to use Git / GitHub in my projects. <br> And I have done coding projects using: <ul> <li>C / C++</li> <li>C#</li> <li>JavaScript</li> <li>Python</li> <li>HTML / CSS</li> </ul> <br> My personal projects include: <ul> <li>Games</li> <li>Android apps</li> <li>Arduino controlled devices, some with custom 3D printed parts</li> <li>Software delopement with and without 3rd party libraries</li> <li>Web developement</li> </ul>'
             break;
+
+        case 3:
+            document.getElementById("card0").innerHTML = 'I have always had an interest towards technology. <br> My source of motivation is my curioty towards different technological applications and what can be achieved using technology as a tool. <br> I am a problem solver and I aim to better myself and constantly improve my skillset with research and practise. <br> I am also relatively easy to work with. I am an introvert but always eager to help others if I can. <br> My greatest weakness is self-doubt.'
+            break;
+
         default:
             cardS();
     }
