16dbafe8572305af6b7fb4bcd7829a1c64f1775b
PyryV <pyry.vehniainen@gmail.com>
2022-09-29 11:28:55 +0300

    Major improvements to JS and HTML code

--- src/script.js
+++ src/script.js
@@ -1,25 +1,36 @@
-function card1f() 
-{
-    if (document.getElementById("card1").style.visibility == "visible")
-    {
-        document.getElementById("card1").style.visibility = "hidden";
-    }
+ function cardS()
+ {
+    if (document.getElementById("card0").style.visibility == "visible" && document.getElementById("cardBase").style.visibility == "visible")
+        {
+            document.getElementById("card0").style.visibility = "hidden";
+            document.getElementById("cardBase").style.visibility = "hidden";
+        }
     else
-    {
-        document.getElementById("card2").style.visibility = "hidden";
-        document.getElementById("card1").style.visibility = "visible";
-    }
+        {
+            document.getElementById("card0").style.visibility = "visible";
+            document.getElementById("cardBase").style.visibility = "visible";
+        }
 }
-
-function card2f() 
+function card0f(num)
 {
-    if (document.getElementById("card2").style.visibility == "visible")
+    document.getElementById("card0").style.visibility = "visible";
+    document.getElementById("cardBase").style.visibility = "visible";
+    switch(num) 
     {
-        document.getElementById("card2").style.visibility = "hidden";
-    }
-    else
-    {
-        document.getElementById("card1").style.visibility = "hidden";
-        document.getElementById("card2").style.visibility = "visible";
+        case 1:
+            //cardS();
+            const T = new Date();
+            let year = T.getFullYear();
+            year = year - 1997;
+            document.getElementById("card0").innerHTML =
+            'Hello, my name is Pyry Vehniäinen. I am a '+ year +' years old software developer. <br> This is my portfolio website. <br> Feel free to look around. <br>';
+            break;
+
+        case 2:
+            //cardS();
+            document.getElementById("card0").innerHTML = 'I know how to use Git / Github in my projects. <br> And I have done coding projects using: <ul> <li>C / C++</li> <li>C#</li> <li>JavaScript</li> <li>Python</li> <li>HTML / CSS</li> </ul>'
+            break;
+        default:
+            cardS();
     }
 }
